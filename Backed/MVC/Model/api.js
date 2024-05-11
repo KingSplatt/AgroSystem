64be3e53371286d02cdec1cc7e98ff911deb7453
KingSplatt <malp18774@gmail.com>
2024-05-10 22:31:42 -0700

    si

--- Backed/MVC/Model/api.js
+++ Backed/MVC/Model/api.js
@@ -6,7 +6,7 @@ const getUsers = async (req,res) =>{
     const [rows, fields] = await pool.query('SELECT * FROM Cliente ' );
     console.log('Usuarios obtenidos:', rows);
     const [rows2, fields2] = await pool.query('SELECT * FROM Ciudad ' );
-    res.status(200).send({success: true,message: "putos", rows, rows2})
+    res.status(200).send({success: true,message: "putos", rows: rows, rows2})
   } catch (err) {
     res.status(500).send({success: false, message: 'Error al obtener usuarios'});
   }
@@ -15,7 +15,7 @@ const getCiudades = async (req,res)=>{
     try {
         const [rows, fields] = await pool.query('SELECT * FROM Ciudad');
         console.log('Ciudades obtenidas:', rows);
-        res.status(200).send({success: true, rows});
+        res.status(200).send({success: true, rows: rows});
     } catch (err) {
         console.error('Error al obtener ciudades:', err);
         res.status(500).send({success: false, message: 'Error al obtener ciudades'});
