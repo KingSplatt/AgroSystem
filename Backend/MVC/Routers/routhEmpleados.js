31764e5b169e3b2fe36c55898361cb3deec25649
KingSplatt <malp18774@gmail.com>
2024-05-12 00:42:19 -0700

    lol

--- Backend/MVC/Routers/routhEmpleados.js
+++ Backend/MVC/Routers/routhEmpleados.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const {  NuevoEmpleado, VerEmpleados, EliminarEmpleado  } = require('../Controller/Empleados');
+const { NuevoEmpleado, VerEmpleados, EliminarEmpleado } = require('../Controller/Empleados');
 const router = express.Router();
 
 router.get('/', VerEmpleados);
 router.post('/', NuevoEmpleado);
 router.delete('/', EliminarEmpleado);
+
+module.exports = router;
--- Backend/MVC/Routers/routhVentaCredito.js
+++ Backend/MVC/Routers/routhVentaCredito.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const {  NuevaVentaCredito, HistorialCreditos  } = require('../Controller/VentaCredito');
+const { NuevaVentaCredito, HistorialCreditos } = require('../Controller/VentaCredito');
 const router = express.Router();
 
 router.get('/', HistorialCreditos);
 router.post('/', NuevaVentaCredito);
+
+module.exports = router;
