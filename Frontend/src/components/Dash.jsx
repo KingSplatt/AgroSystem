6911d1ebbf1b5d76120ba1c57a1b6f694da3f085
Caeneus60 <alanpy60@gmail.com>
2024-05-14 20:43:31 -0700

    Correccion estructura de front

---
+++ Frontend/src/components/Dash.jsx
@@ -0,0 +1,19 @@
+/*import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+//import Dash from './Dash';
+import Sidebar from './Componentes/Sidebar';
+import Inici from './Pages/Inicio.jsx';
+
+const Dash = () => {
+  return (
+    
+    <BrowserRouter>
+        <Sidebar>
+            <Routes>
+                <Route path="/" element={<Inici />} />
+            </Routes>
+        </Sidebar>
+    </BrowserRouter>
+);
+}
+export default Dash;*/
