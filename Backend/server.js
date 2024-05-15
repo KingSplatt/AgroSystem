48ba10e13a4a65fb0e8e859aa26c09cace873a5d
Caeneus60 <alanpy60@gmail.com>
2024-05-14 23:54:15 -0700

    Agrega CORS a backend para permitir conexion con frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 dotenv.config();
 
 //inicias la app
 const app = express();
-app.use(morgan('dev'));
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(morgan("dev"));
 
 app.use(express.json()); //para que entienda los json que le mandamos
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    return res.status(200).send("<h1>hola</h1> <br> <h2>como estas</h2>")
+app.get("/", (req, res) => {
+  return res.status(200).send("<h1>hola</h1> <br> <h2>como estas</h2>");
 });
 
-
 //Routers
-const routerProductos = require('./MVC/Routers/routhProductoSucursal');
-const routerProveedores = require('./MVC/Routers/routhProveedor');
-const routerEmpleados = require('./MVC/Routers/routhEmpleados');
-const routerVentaCredito = require('./MVC/Routers/routhVentaCredito');
-const routerCompra = require('./MVC/Routers/routhCompra');
+const routerProductos = require("./MVC/Routers/routhProductoSucursal");
+const routerProveedores = require("./MVC/Routers/routhProveedor");
+const routerEmpleados = require("./MVC/Routers/routhEmpleados");
+const routerVentaCredito = require("./MVC/Routers/routhVentaCredito");
+const routerCompra = require("./MVC/Routers/routhCompra");
 
-app.use('/productos', routerProductos);
-app.use('/proveedores', routerProveedores);
-app.use('/empleados', routerEmpleados);
-app.use('/VentasC', routerVentaCredito);
-app.use('/compras', routerCompra);
-
+app.use("/productos", routerProductos);
+app.use("/proveedores", routerProveedores);
+app.use("/empleados", routerEmpleados);
+app.use("/VentasC", routerVentaCredito);
+app.use("/compras", routerCompra);
 
 //ES PARA ABRIR EL DOTENV
-const puerto = process.env.PUERTO || 3000;//ABRO MI PUERTO Y SI NO SE ABRE EL 3000
+const puerto = process.env.PUERTO || 3000; //ABRO MI PUERTO Y SI NO SE ABRE EL 3000
 
 app.listen(puerto, () => {
-    console.log(`Servidor corriendo en el puerto ${puerto}`);
+  console.log(`Servidor corriendo en el puerto ${puerto}`);
 });
