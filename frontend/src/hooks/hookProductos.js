0ef790b28cb47d43b6b7dee1b725482af35480c4
Caeneus60 <alanpy60@gmail.com>
2024-05-14 23:55:09 -0700

    Corrige hookProductos para interactuar correctamente con backend.

--- frontend/src/hooks/hookProductos.js
+++ frontend/src/hooks/hookProductos.js
@@ -4,7 +4,7 @@ const useProductos = () => {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
-    fetch("https://localhost:8080/productos")
+    fetch("http://localhost:8080/productos")
       .then((res) => {
         if (!res.ok) {
           throw new Error("Network response was not ok");
@@ -12,8 +12,8 @@ const useProductos = () => {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
-        setProductos(data);
+        console.log(data.rows);
+        setProductos(data.rows);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
--- frontend/src/pages/Productos.jsx
+++ frontend/src/pages/Productos.jsx
@@ -9,28 +9,30 @@ const VerProductos = () => {
   return (
     <div className="table-container">
       <table className="productos-table">
-        <tr>
-          <th>Nombre</th>
-          <th>Descripcion</th>
-          <th>Precio</th>
-          <th>Descontinuado</th>
-          <th>Imagen</th>
-          <th>Proveedor</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+            <th>Precio</th>
+            <th>Descontinuado</th>
+            <th>Imagen</th>
+            <th>Proveedor</th>
+          </tr>
+        </thead>
 
-        {productos.map((producto, i) => {
-          return (
+        <tbody>
+          {productos.map((producto, i) => (
             <Producto
               key={i}
-              nombre={producto.nombre}
-              descripcion={producto.descripcion}
-              precio={producto.precio}
-              descontinuado={producto.descontinuado}
-              imagen={producto.imagen}
-              proveedor={producto.proveedor}
+              nombre={producto.Nombre}
+              descripcion={producto.Descripcion}
+              precio={producto.PrecioUnitario}
+              // Esto da error porque Descontinuado retorna un json.
+              // descontinuado={producto.Descontinuado}
+              proveedor={producto["Nombre Proveedor"]}
             />
-          );
-        })}
+          ))}
+        </tbody>
       </table>
     </div>
   );
