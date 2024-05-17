ba2e4f9c763ab90a76d9341274f649f5a0a3968a
KingSplatt <malp18774@gmail.com>
2024-05-16 21:11:45 -0700

    actualizacion de rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,28 @@ import './App.css';
 //import Dash from './Dash';
 import Sidebar from './Componentes/Sidebar';
 import Login from './Login';
-import ActualizarProveedor from './Pages/ActualizarProveedor.jsx';
-import AnadirProveedor from './Pages/AnadirProveedor.jsx';
-import HistorialCompras from './Pages/HistorialCompras.jsx';
-import Inicio from './Pages/Inicio.jsx';
-import VerProveedores from './Pages/VerProveedores.jsx';
+import ActualizarProveedor from './pages/ActualizarProveedor.jsx';
+import AnadirProveedor from './pages/AnadirProveedor.jsx';
+import HistorialCompras from './pages/HistorialCompras.jsx';
+import Inicio from './pages/Inicio.jsx';
+import VerProveedores from './pages/VerProveedores.jsx';
 
 const App = () => {
 
-  
+
   return (
 
     <BrowserRouter>
-  
-        <Routes>
+
+      <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/inicio" element={<Inicio/>}/>
-        <Route path="/VerProveedores" element={<VerProveedores/>}/>
-        <Route path="/AnadirProveedor" element={<AnadirProveedor/>}/>
-        <Route path="/ActualizarProveedor" element={<ActualizarProveedor/>}/>
-        <Route path="/HistorialCompras" element={<HistorialCompras/>}/>
-        </Routes>
-        <Sidebar nose={"Contenido"} />
+        <Route path="/inicio" element={<Inicio />} />
+        <Route path="/VerProveedores" element={<VerProveedores />} />
+        <Route path="/AnadirProveedor" element={<AnadirProveedor />} />
+        <Route path="/ActualizarProveedor" element={<ActualizarProveedor />} />
+        <Route path="/HistorialCompras" element={<HistorialCompras />} />
+      </Routes>
+      <Sidebar nose={"Contenido"} />
     </BrowserRouter>
   );
 }
