1715ad7aa28a59c05d3ba807f198ccf77d41b20d
KingSplatt <malp18774@gmail.com>
2024-05-19 18:37:57 -0700

    algo de la configuracion :v

--- Backend/MVC/Model/db.config.js
+++ Backend/MVC/Model/db.config.js
@@ -4,7 +4,7 @@ const dbPassword = process.env.DB_PASSWORD || null;
 module.exports = {
   HOST: "localhost", // Or the IP address of your MySQL server
   USER: "root",
-  PASSWORD: "@Disneyxa123",
+  PASSWORD: dbPassword,
   DB: "AgroSystem",
   dialect: "mysql",
 };
