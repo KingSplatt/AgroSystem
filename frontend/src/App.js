791c946e9dde4d42be9e424b60d0a1cd24e15e60
Caeneus60 <alanpy60@gmail.com>
2024-05-19 19:11:54 -0700

    Corrige Pages->pages en App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,59 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Sidebar from './Componentes/Sidebar';
-import Login from './Login';
-import ActualizarProveedor from './Pages/ActualizarProveedor.jsx';
-import AnadirProductos from './Pages/AnadirProductos';
-import AnadirProveedor from './Pages/AnadirProveedor.jsx';
-import Cotizar from './Pages/Cotizar.jsx';
-import HistorialCompras from './Pages/HistorialCompras.jsx';
-import IngresarProductos from './Pages/IngresarProductos';
-import Inicio from './Pages/Inicio.jsx';
-import ModificarProductos from './Pages/ModificarProducto';
-import VerProducto from './Pages/VerProducto';
-import VerProveedores from './Pages/VerProveedores.jsx';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Sidebar from "./Componentes/Sidebar";
+import Login from "./Login";
+import ActualizarProveedor from "./pages/ActualizarProveedor.jsx";
+import AnadirProductos from "./pages/AnadirProductos";
+import AnadirProveedor from "./pages/AnadirProveedor.jsx";
+import Cotizar from "./pages/Cotizar.jsx";
+import HistorialCompras from "./pages/HistorialCompras.jsx";
+import IngresarProductos from "./pages/IngresarProductos";
+import Inicio from "./pages/Inicio.jsx";
+import ModificarProductos from "./pages/ModificarProducto";
+import VerProducto from "./pages/VerProducto";
+import VerProveedores from "./pages/VerProveedores.jsx";
 
 const App = () => {
   return (
-    <div className='Todo'>
+    <div className="Todo">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="*" element={ <>
+          <Route
+            path="*"
+            element={
+              <>
                 <Sidebar />
                 <Routes>
                   <Route path="/inicio" element={<Inicio />} />
                   <Route path="/VerProveedores" element={<VerProveedores />} />
-                  <Route path="/AnadirProveedor" element={<AnadirProveedor />} />
-                  <Route path="/ActualizarProveedor" element={<ActualizarProveedor />} />
-                  <Route path="/HistorialCompras" element={<HistorialCompras />} />
-                  <Route path="/IngresarProductos" element={<IngresarProductos />} />
+                  <Route
+                    path="/AnadirProveedor"
+                    element={<AnadirProveedor />}
+                  />
+                  <Route
+                    path="/ActualizarProveedor"
+                    element={<ActualizarProveedor />}
+                  />
+                  <Route
+                    path="/HistorialCompras"
+                    element={<HistorialCompras />}
+                  />
+                  <Route
+                    path="/IngresarProductos"
+                    element={<IngresarProductos />}
+                  />
                   <Route path="/VerProducto" element={<VerProducto />} />
                   <Route path="/Cotizar" element={<Cotizar />} />
-                  <Route path="/AnadirProductos" element={<AnadirProductos />} />
-                  <Route path="/ModificarProductos" element={<ModificarProductos />} />
+                  <Route
+                    path="/AnadirProductos"
+                    element={<AnadirProductos />}
+                  />
+                  <Route
+                    path="/ModificarProductos"
+                    element={<ModificarProductos />}
+                  />
                 </Routes>
               </>
             }
