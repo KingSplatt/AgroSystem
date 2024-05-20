1f1ff8844b7d2cf40d6c24f15c74323105a436a8
KingSplatt <malp18774@gmail.com>
2024-05-19 18:35:11 -0700

    API Cliente terminada

--- Backend/MVC/Controller/Clientes.js
+++ Backend/MVC/Controller/Clientes.js
@@ -12,4 +12,35 @@ const VerClientes = async (req, res) => {
     }
 }
 
-module.exports = { VerClientes };
+const NuevoCliente = async (req, res) => {
+    try {
+        const { Nombre, ApellidoPaterno, ApellidoMaterno, Usuario, Contrasena, Correo, Telefono, RFC, CURP, Ciudad } = req.body;
+        //obtener IDCiudad
+        const IDCiudad = await pool.query('SELECT IDCiudad FROM Ciudad WHERE Nombre = ?;', [Ciudad]);
+
+        await pool.query('INSERT INTO Cliente (Nombre, ApellidoPaterno, ApellidoMaterno, Usuario, Contrasena, Correo, Telefono, RFC, CURP, IDCiudad) VALUES (?,?,?,?,?);',
+            [Nombre, ApellidoPaterno, ApellidoMaterno, Usuario, Contrasena, Correo, Telefono, RFC, CURP, IDCiudad[0].IDCiudad]);
+        res.status(201).send({ success: true, message: 'Cliente Registrado Correctamente' });
+    } catch (err) {
+        console.error('Error al crear el cliente', err);
+        res.status(500).send({ success: false, message: 'Error al crear el cliente' });
+    }
+}
+
+const ActualizarCliente = async (req, res) => {
+    try {
+        const { Nombre, ApellidoPaterno, ApellidoMaterno, Usuario, Contrasena, Correo, Telefono, RFC, CURP, Ciudad } = req.body;
+        //obtener IDCiudad
+        const IDCiudad = await pool.query('SELECT IDCiudad FROM Ciudad WHERE Nombre = ?;', [Ciudad]);
+        //obtener IDCliente
+        const IDCliente = await pool.query('SELECT IDCliente FROM Cliente WHERE Nombre = ?;', [Nombre]);
+        await pool.query('UPDATE Cliente SET Nombre = ?, ApellidoPaterno = ?, ApellidoMaterno = ?, Usuario = ?, Contrasena = ?, Correo = ?, Telefono = ?, RFC = ?, CURP = ?, IDCiudad = ? WHERE IDCliente = ?;',
+            [Nombre, ApellidoPaterno, ApellidoMaterno, Usuario, Contrasena, Correo, Telefono, RFC, CURP, IDCiudad[0].IDCiudad], IDCliente[0].IDCliente[0]);
+        res.status(201).send({ success: true, message: 'Cliente Actualizado Correctamente' });
+    } catch (err) {
+        console.error('Error al actualizar el cliente', err);
+        res.status(500).send({ success: false, message: 'Error al actualizar el cliente' });
+    }
+}
+
+module.exports = { VerClientes, NuevoCliente, ActualizarCliente };
--- Backend/MVC/Routers/routhClientes.js
+++ Backend/MVC/Routers/routhClientes.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { VerClientes } = require('../Controller/Clientes');
+const { VerClientes, NuevoCliente, ActualizarCliente } = require('../Controller/Clientes');
+
 const router = express.Router();
 
 router.get('/', VerClientes);
+router.post('/', NuevoCliente);
+router.put('/', ActualizarCliente);
 
 module.exports = router;
