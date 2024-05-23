2c29f6252904b661528f036acf339b0c006b6594
jacq1813 <l21170454@culiacan.tecnm.mx>
2024-05-22 22:04:37 -0700

    cotizacionConexion

--- frontend/src/pages/Cotizar.jsx
+++ frontend/src/pages/Cotizar.jsx
@@ -1,110 +1,180 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaRegSave, FaRegTimesCircle } from "react-icons/fa";
 import '../Estilos/Cotizar.css';
 
-const MiComponente = () => {
-  const [proveedor, setProveedor] = useState('');
-  const [producto, setProducto] = useState('');
+const URL = 'http://localhost:8080/productosCEDI';
+const URI = 'http://localhost:8080/proveedores';
+
+const Cotizar = () => {
+  const [nuevoProveedor, setNuevoProveedor] = useState('');
+  const [nuevoProducto, setNuevoProducto] = useState('');
+
   const [proveedores, setProveedores] = useState([]);
   const [productos, setProductos] = useState([]);
 
-  const handleProveedorChange = (e) => {
-    setProveedor(e.target.value);
+  const [proveedors, setProveedors] = useState([]);
+  const [productors, setProductors] = useState([]);
+
+  useEffect(() => { 
+    fetchProveedores();
+    fetchProductos();
+  }, []);
+
+  const fetchProveedores = async () => {
+    try {
+      const response = await fetch(URI);
+      const data = await response.json();
+      const rows = data.rows;
+
+      if (Array.isArray(rows)) {
+        setProveedores(rows);
+      } else {
+        console.log('La respuesta no es un array', data);
+        alert('Error al obtener los datos de los proveedores');
+      }
+    } catch (error) {
+      console.error('Error al obtener los datos de los proveedores', error);
+      alert('Error al obtener los datos de los proveedores');
+    }
   };
 
-  const handleProductoChange = (e) => {
-    setProducto(e.target.value);
+  const fetchProductos = async () => { 
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      const rows = data.rows;
+
+      if (Array.isArray(rows)) {
+        setProductos(rows);
+      } else {
+        console.log('La respuesta no es un array', data);
+        alert('Error al obtener los datos de los productos');
+      }
+    } catch (error) {
+      console.error('Error al obtener los datos de los productos', error);
+      alert('Error al obtener los datos de los productos');
+    }
   };
+
+  const handleProveedorChange = (event) => {
+    setNuevoProveedor(event.target.value);
+  };
+
+  const handleProductoChange = (event) => {
+    setNuevoProducto(event.target.value);
+  };
+
 
   const agregarProveedor = () => {
-    if (proveedor.trim()) {
-      setProveedores([...proveedores, proveedor.trim()]);
-      setProveedor('');
+    if (nuevoProveedor.trim() === '') {
+      alert('El campo proveedor no puede estar vacío');
+      return;
+    }
+    const proveedor = proveedores.find((p) => p.IDProveedor === parseInt(nuevoProveedor));
+
+    if (proveedor) {
+      setProveedors([...proveedors, proveedor]);
+      setNuevoProveedor('');
+    } else {
+      alert('Proveedor no encontrado. Intente de nuevo');
     }
   };
 
   const agregarProducto = () => {
-    if (producto.trim()) {
-      setProductos([...productos, producto.trim()]);
-      setProducto('');
+    if (nuevoProducto.trim() === '') {
+      alert('El campo producto no puede estar vacío');
+      return;
+    }
+
+    const producto = productos.find((p) => p.IDProducto === parseInt(nuevoProducto));
+
+    console.log('nuevoProducto:', nuevoProducto);
+    console.log('producto:', producto);
+    console.log(productos);
+    
+    if (producto) {
+      setProductors([...productors, producto]);
+      setNuevoProducto('');
+    } else {
+      alert('Producto no encontrado. Intente de nuevo');
     }
   };
 
   const eliminarProveedor = (index) => {
-    const nuevosProveedores = [...proveedores];
-    nuevosProveedores.splice(index, 1);
-    setProveedores(nuevosProveedores);
+    const newProveedors = proveedors.filter((_, i) => i !== index);
+    setProveedors(newProveedors);
   };
 
-    const eliminarProducto = (index) => {
-    const nuevosProductos = [...productos];
-    nuevosProductos.splice(index, 1);
-    setProductos(nuevosProductos);
-    };
-
+  const eliminarProducto = (index) => {
+    const newProductors = productors.filter((_, i) => i !== index);
+    setProductors(newProductors);
+  };
+  
   return (
     <div className='ContenedorC'>
-    <div className='Completo'>
-    <h1>Cotizar</h1>
-    <div className='Superior'>
-      <div>
-        <label>Proveedor:</label>
-        <input type="text" value={proveedor} onChange={handleProveedorChange} />
-        <button onClick={agregarProveedor}>Añadir proveedor</button>
-      </div>
-      <div>
-        <label>Producto:</label>
-        <input type="text" value={producto} onChange={handleProductoChange} />
-        <button onClick={agregarProducto}>Añadir producto</button>
-      </div>
-    </div>
-    <div className='Inferior'>
-      <div>
-        <h3>Proveedores:</h3>
-        <div className='CotizarProveedores' >
-          {proveedores.map((p, index) => (
-            <label key={index}>
-              {p}
-              <FaRegTimesCircle onClick={() => eliminarProveedor(index)}
-              style={{ cursor: 'pointer', marginLeft: '8px' }} />
-            </label>
-          ))}
-      </div>
-      </div>
-      <div>
-        <h3>Productos:</h3>
-        <table className='Tabla'>
-          
-            <thead>
-                <tr>
-                    <th>Clave</th>
-                    <th>Producto</th>
-                </tr>
-            </thead>
-   
-        <tbody>
-            {productos.map((p, index) => (
-           <tr key={index}> 
-                <td>{p}
-                    <FaRegTimesCircle
-                    onClick={() => eliminarProducto(index)}
-                    style={{ cursor: 'pointer', marginLeft: '8px' }}
-                    />
-                </td>
-            </tr>
-          ))}
-        </tbody>
-        </table>
-      </div>
-    </div>
-    </div>
-
-    <div className="CyG">
-           <button className="Cancel"><FaRegTimesCircle /> Cancelar</button>
-           <button className= "Save"><FaRegSave /> Guardar</button>
+      <div className='Completo'>
+        <h1>Cotizar</h1>
+        <div className='Superior'>
+          <div>
+            <label>Proveedor: </label>
+            <input type="text" value={nuevoProveedor} onChange={handleProveedorChange}  />
+            <button onClick={agregarProveedor} >Añadir proveedor</button>
+          </div>
+          <div>
+            <label>Producto: </label>
+            <input type="text" value={nuevoProducto} onChange={handleProductoChange} />
+            <button onClick={agregarProducto}>Añadir producto</button>
+          </div>
         </div>
+        <div className='Inferior'>
+          <div>
+            <h3>Proveedores: </h3>
+            <div className='CotizarProveedores'>
+              {proveedors.map((p, index) => (
+                <label key={index}>
+                  {p.Nombre}
+                  <FaRegTimesCircle
+                    onClick={() => eliminarProveedor(index)}
+                    style={{ cursor: 'pointer', marginLeft: '8px' }}
+                  />
+                </label>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h3>Productos: </h3>
+            <table className='Tabla'>
+              <thead>
+                <tr>
+                  <th>Clave</th>
+                  <th>Producto</th>
+                  <th>Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {productors.map((p, index) => (
+                  <tr key={index}>
+                    <td>{p.IDProducto}</td>
+                    <td>{p.Nombre}</td>
+                    <td>
+                      <FaRegTimesCircle
+                        onClick={() => eliminarProducto(index)}
+                        style={{ cursor: 'pointer', textAlign: 'center'}}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div className="CyG">
+        <button className="Cancel"><FaRegTimesCircle /> Cancelar</button>
+        <button className="Save"><FaRegSave /> Guardar</button>
+      </div>
     </div>
   );
 };
 
-export default MiComponente;
+export default Cotizar;
