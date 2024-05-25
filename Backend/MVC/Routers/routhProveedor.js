9128b007e7fe8d1b5fe5d2c277e81d6af0ce493a
Audelo03 <diegoaudelopersonal@gmail.com>
2024-05-24 23:57:33 -0700

    refactor: Add route to obtain provider by clave

--- Backend/MVC/Routers/routhProveedor.js
+++ Backend/MVC/Routers/routhProveedor.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { ObtenerProveedor, agregarProveedor, EliminarProveedor } = require('../Controller/Proveedores');
+const { ObtenerProveedor, agregarProveedor, EliminarProveedor,ObtenerProveedorByClave } = require('../Controller/Proveedores');
 const router = express.Router();
 
 router.get('/', ObtenerProveedor);
 router.post('/', agregarProveedor);
 router.delete('/', EliminarProveedor);
+router.get('/:clave', ObtenerProveedorByClave);
+
 
 module.exports = router;
