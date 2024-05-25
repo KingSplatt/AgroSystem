d3a1c321b60226bd5873a7b866e2910a4f3e62fb
jacq1813 <l21170454@culiacan.tecnm.mx>
2024-05-25 15:06:35 -0700

    estilos

--- frontend/src/pages/IngresarProductos.jsx
+++ frontend/src/pages/IngresarProductos.jsx
@@ -1,45 +1,61 @@
 import React from 'react';
+import { FaRegSave, FaRegTimesCircle } from 'react-icons/fa';
 import '../Estilos/IngresarProductos.css';
 
 
 const IngresarProductos = () => {
 
-    return(
+    const Cancelar = () => {
+        //limpiar tabla y formulario
+        document.getElementById('claveCompra').value = '';
+        document.getElementById('articulo').value = '';
+        document.getElementById('cantidad').value = '';
+        document.getElementById('precio').value = '';
+        document.getElementById('proveedor').value = '';
+        document.querySelector('tbody').innerHTML = '';
+        document.querySelector('tfoot td:last-child').textContent = '0';
 
 
-        <div className='IngresarProductos'>
+    }
 
-            <h1>Ingresar Productos</h1>
+
+    return (
+
+        <div className='todoIG'>
+
+            <div className='IngresarProductos'>
+
+                <h2>Ingresar Productos</h2>
 
                 <div className='FormularioProducto'>
 
                     <form className='FormularioIngreso'>
-                    <div className='TopIngreso'>
-                        <label>Proveedor: </label>
-                        <select id='proveedor'>
-                            <option value=''>Seleccionar: </option>
-                            {/* Opciones de proveedor */}
-                        </select>
+                        <div className='TopIngreso'>
+                            <label>Proveedor: </label>
+                            <select id='proveedor'>
+                                <option value=''>Seleccionar: </option>
+                                {/* Opciones de proveedor */}
+                            </select>
 
-                        <label>Clave de compra: </label>
-                        <input type='text' id='claveCompra' />
-                    </div>
+                            <label>Clave de compra: </label>
+                            <input type='text' id='claveCompra' />
+                        </div>
 
 
-                    <div className='MsjIngreso'><label><b>Detalles </b> </label> <hr></hr></div>
-                    <div className= 'BottomIngreso'>
+                        <div className='MsjIngreso'><label><b>Detalles </b> </label> <hr></hr></div>
+                        <div className='BottomIngreso'>
 
-                        <label>Articulo: </label>
-                        <input type='text' id='articulo' />
+                            <label>Articulo: </label>
+                            <input type='text' id='articulo' />
 
-                        <label>Cantidad: </label>
-                        <input type='number' id='cantidad' />
+                            <label>Cantidad: </label>
+                            <input type='number' id='cantidad' />
 
-                        <label>Precio: </label>
-                        <input type='number' id='precio' />
-                    </div>
+                            <label>Precio: </label>
+                            <input type='number' id='precio' />
+                        </div>
 
-                    <button className='AgregarATable'>Ingresar</button>
+                        <button className='AgregarATable'>Ingresar</button>
                     </form>
 
 
@@ -60,19 +76,24 @@ const IngresarProductos = () => {
                         <tbody>
                             {/* Aquí se llenará la tabla */}
                         </tbody>
-                    <tfoot>
-                        <tr>
-                            <td>Total: </td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td>0</td>
-                        </tr>
-                    </tfoot>
+                        <tfoot>
+                            <tr>
+                                <td>Total: </td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>0</td>
+                            </tr>
+                        </tfoot>
                     </table>
                 </div>
 
+                <div className="CyG">
+                    <button className="Cancel" onClick={Cancelar}><FaRegTimesCircle /> Cancelar</button>
+                    <button className="Save"><FaRegSave /> Guardar</button>
+                </div>
+            </div>
         </div>
     )
 
