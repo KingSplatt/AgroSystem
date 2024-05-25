7342e14903d1a773284751b99dd1dba14e4435a8
Caeneus60 <alanpy60@gmail.com>
2024-05-24 17:09:43 -0700

    Agrega interaccion entre frontend y backend para AnadirProveedor.

--- Backend/MVC/Controller/Proveedores.js
+++ Backend/MVC/Controller/Proveedores.js
@@ -4,49 +4,84 @@ const pool = require("../Model/Connection");
 */
 //agregar un proveedor nuevo
 const agregarProveedor = async (req, res) => {
-    try {
-        const { Nombre, RFC, CURP, Ciudad, Correo, Telefono, Legalizado } = req.body;
-        if (!Nombre || !RFC || !CURP || !Ciudad || !Correo || !Telefono || typeof Legalizado === undefined) {
-            return res.status(400).send({ success: false, message: 'Faltan campos por llenar' });
-        }
-        const LegalizadoValue = Legalizado === "Si" ? 1 : 0;
-        const [IDProveedor, fields] = await pool.query('SELECT COUNT(IDProveedor)+1 AS IDProveedor FROM Proveedor');
-        const [IDCiudad, campos] = await pool.query('SELECT IDCiudad FROM Ciudad WHERE Nombre = ?;', [Ciudad]);
-        const insertSQL = 'INSERT INTO Proveedor (IDProveedor, Nombre, Telefono, Correo, RFC, CURP, Legalizado, IDCiudad) VALUES (?,?,?,?,?,?,?,?);';
-        const insertResult = await pool.query(insertSQL, [parseInt(IDProveedor[0].IDProveedor), Nombre, Telefono, Correo, RFC, CURP, LegalizadoValue, parseInt(IDCiudad[0].IDCiudad)]);
-        console.log('Proveedor agregado:', insertResult);
-        res.status(201).send({ success: true, message: "Proveedor Añadido" });
-
-    } catch (err) {
-        console.error('Error al agregar proveedor:', err);
-        res.status(500).send({ success: false, message: 'Error al querer agregar un proveedor' });
+  try {
+    const { Nombre, RFC, CURP, Ciudad, Correo, Telefono, Legalizado } =
+      req.body;
+    if (
+      !Nombre ||
+      !RFC ||
+      !CURP ||
+      !Ciudad ||
+      !Correo ||
+      !Telefono ||
+      !Legalizado
+    ) {
+      return res.status(400).send({
+        success: false,
+        message: "Faltan campos por llenar",
+      });
     }
-}
+    const LegalizadoValue = parseInt(Legalizado);
+    const [IDProveedor, fields] = await pool.query(
+      "SELECT COUNT(IDProveedor)+1 AS IDProveedor FROM Proveedor"
+    );
+    const [IDCiudad, campos] = await pool.query(
+      "SELECT IDCiudad FROM Ciudad WHERE Nombre = ?;",
+      [Ciudad]
+    );
+    const insertSQL =
+      "INSERT INTO Proveedor (IDProveedor, Nombre, Telefono, Correo, RFC, CURP, Legalizado, IDCiudad) VALUES (?,?,?,?,?,?,?,?);";
+    const insertResult = await pool.query(insertSQL, [
+      parseInt(IDProveedor[0].IDProveedor),
+      Nombre,
+      Telefono,
+      Correo,
+      RFC,
+      CURP,
+      LegalizadoValue,
+      parseInt(IDCiudad[0].IDCiudad),
+    ]);
+    console.log("Proveedor agregado:", insertResult);
+    res.status(201).send({ success: true, message: "Proveedor Añadido" });
+  } catch (err) {
+    console.error("Error al agregar proveedor:", err);
+    res.status(500).send({
+      success: false,
+      message: "Error al querer agregar un proveedor",
+    });
+  }
+};
 
 //Obtener un proveedor de la lista
 const ObtenerProveedor = async (req, res) => {
-    try {
-        const [rows, fields] = await pool.query('SELECT * FROM Proveedor');
-        console.log('Proveedores:', rows);
-        res.status(200).send({ success: true, rows: rows });
-    } catch (err) {
-        console.error('Error al obtener ciudades:', err);
-        res.status(500).send({ success: false, message: 'Error al querer obtener los proveedores' });
-    }
-}
+  try {
+    const [rows, fields] = await pool.query("SELECT * FROM Proveedor");
+    console.log("Proveedores:", rows);
+    res.status(200).send({ success: true, rows: rows });
+  } catch (err) {
+    console.error("Error al obtener ciudades:", err);
+    res.status(500).send({
+      success: false,
+      message: "Error al querer obtener los proveedores",
+    });
+  }
+};
 
 //Eliminar un proveedor de la lista
 const EliminarProveedor = async (req, res) => {
-    try {
-        const { IDProveedor } = req.body;
-        const deleteSQL = 'DELETE FROM Proveedor WHERE IDProveedor = ?';
-        const deleteResult = await pool.query(deleteSQL, [parseInt(IDProveedor)]);
-        console.log('Proveedor eliminado:', deleteResult);
-        res.status(201).send({ success: true, message: "Proveedor eliminado" });
-    } catch (err) {
-        console.error('Error al eliminar proveedor:', err);
-        res.status(500).send({ success: false, message: 'Error al querer eliminar un proveedor' });
-    }
-}
+  try {
+    const { IDProveedor } = req.body;
+    const deleteSQL = "DELETE FROM Proveedor WHERE IDProveedor = ?";
+    const deleteResult = await pool.query(deleteSQL, [parseInt(IDProveedor)]);
+    console.log("Proveedor eliminado:", deleteResult);
+    res.status(201).send({ success: true, message: "Proveedor eliminado" });
+  } catch (err) {
+    console.error("Error al eliminar proveedor:", err);
+    res.status(500).send({
+      success: false,
+      message: "Error al querer eliminar un proveedor",
+    });
+  }
+};
 
-module.exports = { agregarProveedor, ObtenerProveedor, EliminarProveedor }
+module.exports = { agregarProveedor, ObtenerProveedor, EliminarProveedor };
--- frontend/src/pages/AnadirProveedor.jsx
+++ frontend/src/pages/AnadirProveedor.jsx
@@ -1,24 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { FaRegSave, FaRegTimesCircle, } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import { FaRegSave, FaRegTimesCircle } from "react-icons/fa";
 import "../Estilos/AddProveedores.css";
 
 const URI_Proveedores = "http://localhost:8080/proveedores";
 const URI_Ciudades = "http://localhost:8080/ciudades";
 const URI_Estados = "http://localhost:8080/estados";
 
-
 const AnadirProveedor = () => {
   const [Proveedores, setProveedores] = useState([]);
   const [Ciudades, setCiudades] = useState([]);
   const [Estados, setEstados] = useState([]);
   const [formProveedores, setFormProveedores] = useState({
-    nombre: "",
-    rfc: "",
-    curp: "",
-    ciudad: "",
-    correo: "",
-    telefono: "",
-    legalizado: "",
+    Nombre: "",
+    RFC: "",
+    CURP: "",
+    Ciudad: "",
+    Correo: "",
+    Telefono: "",
+    Legalizado: "",
   });
 
   useEffect(() => {
@@ -44,12 +43,9 @@ const AnadirProveedor = () => {
       const Ciudades = await response.json();
       const rows = Ciudades.rows;
 
-      console.log("Ciudades:", Ciudades.rows);
-
       if (Array.isArray(rows)) {
         setCiudades(rows);
       } else {
-        console.error("La respuesta no es un array", Ciudades);
         alert("Error al obtener las ciudades: la respuesta no es un array");
       }
     } catch (error) {
@@ -66,21 +62,15 @@ const AnadirProveedor = () => {
       const Estados = await response.json();
       const rows = Estados.rows;
 
-      console.log("Estados:", Estados.rows);
-
       if (Array.isArray(rows)) {
         setEstados(rows);
       } else {
-        console.error("La respuesta no es un array", Estados);
         alert("Error al obtener los estados: la respuesta no es un array");
       }
     } catch (error) {
-      console.error("Error al obtener los estados:", error);
       alert("Error al obtener los estados:", error);
     }
   };
-
-
 
   const handleChange = (e) => {
     const { id, value } = e.target;
@@ -90,6 +80,7 @@ const AnadirProveedor = () => {
   //funcion para enviar los datos
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(formProveedores);
     try {
       const response = await fetch(URI_Proveedores, {
         method: "POST",
@@ -99,12 +90,7 @@ const AnadirProveedor = () => {
         body: JSON.stringify(formProveedores),
       });
       const data = await response.json();
-      if (data.success) {
-        alert("Proveedor añadido correctamente");
-        fetchProveedores();
-      } else {
-        alert("Error al añadir proveedor");
-      }
+      console.log(data);
     } catch (error) {
       console.error("Error al añadir proveedor:", error);
       alert("Error al añadir proveedor:", error);
@@ -115,70 +101,100 @@ const AnadirProveedor = () => {
     <div className="formularioAP">
       <h2>Añadir proveedor</h2>
 
-
       <form className="ADDP">
-
         <div className="grupo1">
           <div className="form-group">
             <label htmlFor="nombre">Nombre: </label>
-            <input type="text" id="nombre" value={formProveedores.nombre} onChange={handleChange} />
+            <input
+              type="text"
+              id="Nombre"
+              value={formProveedores.Nombre}
+              onChange={handleChange}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="rfc">RFC: </label>
-            <input type="text" id="rfc" value={formProveedores.rfc} onChange={handleChange} />
+            <input
+              type="text"
+              id="RFC"
+              value={formProveedores.RFC}
+              onChange={handleChange}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="curp">CURP: </label>
-            <input type="text" id="curp" value={formProveedores.curp} onChange={handleChange} />
+            <input
+              type="text"
+              id="CURP"
+              value={formProveedores.CURP}
+              onChange={handleChange}
+            />
           </div>
-
         </div>
 
         <div className="grupo2">
-
           <div className="form-group">
             <label htmlFor="ciudad">Ciudad: </label>
-            <select id="ciudad">
+            <select
+              id="Ciudad"
+              onChange={handleChange}
+              value={formProveedores.Ciudad}
+            >
               <option value="">Seleccionar: </option>
               {Ciudades.map((ciudad, index) => (
-                <option key={index} value={ciudad.IDCiudad}>{ciudad.Nombre}</option>
+                <option key={index} value={ciudad.Nombre}>
+                  {ciudad.Nombre}
+                </option>
               ))}
             </select>
           </div>
 
           <div className="form-group">
             <label htmlFor="correo">Correo: </label>
-            <input type="email" id="correo" value={formProveedores.correo} onChange={handleChange} />
+            <input
+              type="email"
+              id="Correo"
+              value={formProveedores.Correo}
+              onChange={handleChange}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="telefono">Telefono: </label>
-            <input type="text" id="telefono" value={formProveedores.telefono} onChange={handleChange} />
+            <input
+              type="text"
+              id="Telefono"
+              value={formProveedores.Telefono}
+              onChange={handleChange}
+            />
           </div>
-
         </div>
 
         <div className="grupo3">
-
           <div className="form-group">
             <label htmlFor="legalizado">Legalizado: </label>
-            <select id="legalizado">
+            <select
+              id="Legalizado"
+              value={formProveedores.Legalizado}
+              onChange={handleChange}
+            >
               <option value="">Seleccionar: </option>
               <option value="1">Si</option>
               <option value="0">No</option>
             </select>
           </div>
-
         </div>
-
 
         <div className="CyG">
-          <button className="Cancel"><FaRegTimesCircle /> Cancelar</button>
-          <button className="Save" onClick={handleSubmit}><FaRegSave /> Guardar</button>
+          <button className="Cancel">
+            <FaRegTimesCircle /> Cancelar
+          </button>
+          <button className="Save" onClick={handleSubmit}>
+            <FaRegSave /> Guardar
+          </button>
         </div>
-
       </form>
     </div>
   );
